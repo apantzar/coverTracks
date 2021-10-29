2eacd0b0cf7b6b668c22a147b4f449dd2ee54e70
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2021-10-29 22:34:57 +0300

    Init

---
+++ coverMyTracks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import subprocess
+print("Installing packages for you...")
+print()
+subprocess.call("sudo apt-get install python3-tk", shell = True)
+subprocess.call("sudo apt-get install python3-tk", shell= True)
+import Tkinter as tk
+
+top = tk.Tk()
