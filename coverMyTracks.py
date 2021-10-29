d6002e334eeb5301612ee5c5df1d7de79153bf27
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2021-10-29 23:15:32 +0300

    BASICS

--- coverMyTracks.py
+++ coverMyTracks.py
@@ -1,9 +1,44 @@
 #!/usr/bin/env python
 import subprocess
+from tkinter.constants import BOTTOM
 print("Installing packages for you...")
 print()
 subprocess.call("sudo apt-get install python3-tk", shell = True)
 subprocess.call("sudo apt-get install python3-tk", shell= True)
-import Tkinter as tk
+import tkinter as tk
+changeStat = ""
+
+
+def cover():
+    global changeStat
+    changeStat="Relax, I am going to delete everything..."
+    label.config(text=changeStat)
+
+
+    subprocess.call("echo "" > /var/log/auth.log ", shell = True)
+    subprocess.call("echo "" > ~/ .bash_history", shell = True)
+    subprocess.call("rm ~/ .bash_history -rf", shell = True)
+    subprocess.call("history -c", shell = True)
+    subprocess.call("export HISTFILESIZE=0", shell = True)
+    subprocess.call("export HISTSIZE=0", shell = True)
+    subprocess.call("unset HISTFILE", shell = True)
+    subprocess.call("ln /dev/null ~/ .bash_history -sf", shell = True)
+
+
+
 
 top = tk.Tk()
+top.geometry('400x300')
+destroy = tk.Button(text="Destroy Tracks", fg='blue' , width=400, command=cover)
+destroy.grid(row = 0, column = 3, padx = 100)
+destroy.pack(side=BOTTOM)
+
+label = tk.Label(text="Clearing auth.log file...", name="label-status" )
+label.pack()
+
+
+top.mainloop()
+
+
+
+
